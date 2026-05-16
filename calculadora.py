f0fcae706e3c283d2dc9d20e94b1bc51ee3c1718
colomboamd-glitch <colombo.amd@gmail.com>
2026-05-16 23:15:02 +0000

    codigos do primeiro dia de aula

--- calculadora.py
+++ calculadora.py
@@ -1,8 +1,8 @@
-valor = []
+
 
 numeroUm = int(input("digite o primeiro numero "))
 numeroDois = int(input("digite o segundo numero "))
 total = numeroUm + numeroDois
 
 
-print (f"o resultado entre {numeroUm} e {numeroDois} é: ")
+print (f"o resultado entre {numeroUm} e {numeroDois} é {total} ")
